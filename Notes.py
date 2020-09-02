bec45c35e7e09d7e1d78cfa2be761621e3f24255
Ruiner85 <paul.m.mackay85@gmail.com>
2020-09-02 21:18:23 +0100

    The Dictionary Data Type

--- Notes.py
+++ Notes.py
@@ -1,4 +1,3 @@
-# Notes
+# Module 7 - Dictionaries
 
-# Watched Ned Batchelder's "Facts and Myths about Python names and values" talk
-# from PyCon 2015 on YouTube - https://www.youtube.com/watch?v=_AEJHKGk9ns
+# Watched Ned Batchelder's "Facts and Myths about Python names and values" talk from PyCon 2015 on YouTube - https://www.youtube.com/watch?v=_AEJHKGk9ns - which was suggested at the end of Module 6.
